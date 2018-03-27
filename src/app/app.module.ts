5d9951e9bae8847b63df01526726a5b6f9ee63dc
nick19750629 <34128649+nick19750629@users.noreply.github.com>
2018-03-27 19:14:36 +0800

    2018/3/27 第四章依赖注入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,17 @@ import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { StarsComponent } from './stars/stars.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {ProductService} from "./shared/product.service";
 
+const routeConfig:Routes = [
+  {path:'',component:HomeComponent},
+  {path:'product/:productId',component:ProductDetailComponent}
+];
 
 @NgModule({
   declarations: [
@@ -19,12 +29,16 @@ import { StarsComponent } from './stars/stars.component';
     SearchComponent,
     CarouselComponent,
     ProductComponent,
-    StarsComponent
+    StarsComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routeConfig)
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
